c9d482f0d8090e85c899933a9d412b794db2b228
bijay.shrestha <bijay.shrestha@f1soft.com>
2021-06-28 15:20:13 +0545

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dear Akbar dai & Pravesh dai,
+          <br/>
+          This is Bijay Shrestha hosting React app from cloudflare.
         </p>
         <a
           className="App-link"
